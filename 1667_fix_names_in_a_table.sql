901b4a41c91604cba044fbe373ef8573aa4fc279
slakingex@gmail.com <slakingex@gmail.com>
2022-08-09 23:09:50 +0900

    day1: 9 solved

---
+++ 1667_fix_names_in_a_table.sql
@@ -0,0 +1,48 @@
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+
+# My Answer
+SELECT user_id, CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2))) as name
+FROM Users
+ORDER BY user_id ASC
+
+
+# What I Learned
+1) For mySQL, you have to use CONCAT({STRING1}, {STRING2}...)
+2) INDEX for SUBSTR starts from 1 (not 0)
