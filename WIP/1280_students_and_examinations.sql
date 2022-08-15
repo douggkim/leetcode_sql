1677ef6a1b2ecd8818b17681af086d1016ecd301
slakingex@gmail.com <slakingex@gmail.com>
2022-08-15 22:13:44 +0900

    Day 7 : Sovled 1 (Total 49)

---
+++ WIP/1280_students_and_examinations.sql
@@ -0,0 +1,128 @@
+Table: Students
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| student_name  | varchar |
++---------------+---------+
+student_id is the primary key for this table.
+Each row of this table contains the ID and the name of one student in the school.
+ 
+
+Table: Subjects
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| subject_name | varchar |
++--------------+---------+
+subject_name is the primary key for this table.
+Each row of this table contains the name of one subject in the school.
+ 
+
+Table: Examinations
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| student_id   | int     |
+| subject_name | varchar |
++--------------+---------+
+There is no primary key for this table. It may contain duplicates.
+Each student from the Students table takes every course from the Subjects table.
+Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+ 
+
+Write an SQL query to find the number of times each student attended each exam.
+
+Return the result table ordered by student_id and subject_name.
+
+The query result format is in the following example.
+
+SELECT E.student_id, S.student_name, E.subject_name, IFNULL(COUNT(*),0) attended_exams
+FROM Students S, Examinations E 
+WHERE E.student_id = S.student_id
+GROUP BY E.student_id, E.subject_name
+ 
+
+Example 1:
+
+Input: 
+Students table:
++------------+--------------+
+| student_id | student_name |
++------------+--------------+
+| 1          | Alice        |
+| 2          | Bob          |
+| 13         | John         |
+| 6          | Alex         |
++------------+--------------+
+Subjects table:
++--------------+
+| subject_name |
++--------------+
+| Math         |
+| Physics      |
+| Programming  |
++--------------+
+Examinations table:
++------------+--------------+
+| student_id | subject_name |
++------------+--------------+
+| 1          | Math         |
+| 1          | Physics      |
+| 1          | Programming  |
+| 2          | Programming  |
+| 1          | Physics      |
+| 1          | Math         |
+| 13         | Math         |
+| 13         | Programming  |
+| 13         | Physics      |
+| 2          | Math         |
+| 1          | Math         |
++------------+--------------+
+Output: 
++------------+--------------+--------------+----------------+
+| student_id | student_name | subject_name | attended_exams |
++------------+--------------+--------------+----------------+
+| 1          | Alice        | Math         | 3              |
+| 1          | Alice        | Physics      | 2              |
+| 1          | Alice        | Programming  | 1              |
+| 2          | Bob          | Math         | 1              |
+| 2          | Bob          | Physics      | 0              |
+| 2          | Bob          | Programming  | 1              |
+| 6          | Alex         | Math         | 0              |
+| 6          | Alex         | Physics      | 0              |
+| 6          | Alex         | Programming  | 0              |
+| 13         | John         | Math         | 1              |
+| 13         | John         | Physics      | 1              |
+| 13         | John         | Programming  | 1              |
++------------+--------------+--------------+----------------+
+Explanation: 
+The result table should contain all students and all subjects.
+Alice attended the Math exam 3 times, the Physics exam 2 times, and the Programming exam 1 time.
+Bob attended the Math exam 1 time, the Programming exam 1 time, and did not attend the Physics exam.
+Alex did not attend any exams.
+John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.
+
+
+-- My Answer 
+
+WITH int_table AS 
+(SELECT student_id, student_name, subject_name 
+FROM Subjects, Students), 
+enroll_count_table AS 
+(SELECT E.student_id, E.subject_name, COUNT(*) AS attended_exams
+FROM Examinations E
+GROUP BY E.student_id, E.subject_name)
+
+SELECT I.student_id, I.student_name, I.subject_name, IFNULL(E.attended_exams,0) attended_exams
+FROM int_table I 
+LEFT OUTER JOIN enroll_count_table E 
+ON E.student_id = I.student_id
+AND E.subject_name = I.subject_name
+ORDER BY I.student_id, I.subject_name
+
+-- What I Learned 
+1) Use CROSS JOIN (SELECT FROM WITHOUT WHERE CLAUSE) to get a list of all the students & subjects 
