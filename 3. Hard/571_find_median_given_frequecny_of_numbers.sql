c3d354ea7b2222e06048cb2bdd20a9157ad9b488
slakingex@gmail.com <slakingex@gmail.com>
2022-09-06 15:42:46 -0700

    Day 24: Solved 4 (Total 118)

---
+++ 3. Hard/571_find_median_given_frequecny_of_numbers.sql
@@ -0,0 +1,54 @@
+Table: Numbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
+| frequency   | int  |
++-------------+------+
+num is the primary key for this table.
+Each row of this table shows the frequency of a number in the database.
+ 
+
+The median is the value separating the higher half from the lower half of a data sample.
+
+Write an SQL query to report the median of all the numbers in the database after decompressing the Numbers table. Round the median to one decimal point.
+
+The query result format is in the following example.
+
+
+Example 1:
+
+Input: 
+Numbers table:
++-----+-----------+
+| num | frequency |
++-----+-----------+
+| 0   | 7         |
+| 1   | 1         |
+| 2   | 3         |
+| 3   | 1         |
++-----+-----------+
+Output: 
++--------+
+| median |
++--------+
+| 0.0    |
++--------+
+Explanation: 
+If we decompress the Numbers table, we will get [0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 3], so the median is (0 + 0) / 2 = 0.
+
+
+-- My Answer 
+WITH AccumTable AS 
+(SELECT num, SUM(frequency) OVER (ORDER BY num ASC) AS accumul, SUM(frequency) OVER () AS total, frequency
+FROM Numbers Num) 
+
+
+
+SELECT ROUND(AVG(AT.num),1) AS median 
+FROM AccumTable AT
+WHERE AT.total/2 <= AT.accumul
+ AND AT.total/2 >= AT.accumul - frequency
+
+
